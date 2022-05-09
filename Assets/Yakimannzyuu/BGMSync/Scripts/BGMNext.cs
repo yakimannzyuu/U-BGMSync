4c7602a1f78a48f751154797a6687fb86835445a
yakimannzyuu <yakimannzyuu.vrc@gmail.com>
2022-05-09 15:03:28 +0900

    Version 1

---
+++ Assets/Yakimannzyuu/BGMSync/Scripts/BGMNext.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BGMNext : UdonSharpBehaviour
+{
+    [SerializeField] BGMSync sync;
+    
+    public override void Interact()
+    {
+        sync.BgmNumber++;
+    }
+}
